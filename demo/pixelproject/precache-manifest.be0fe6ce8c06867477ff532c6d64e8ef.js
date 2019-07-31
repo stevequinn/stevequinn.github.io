5d36fbd5ac98c6e81959bcb71093307601836e30
Steve Quinn <steve@sq.net.au>
2019-07-31 18:53:47 +0200

    Adds demo pixel project

---
+++ demo/pixelproject/precache-manifest.be0fe6ce8c06867477ff532c6d64e8ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343e7c7de5558b4d6e0137769d74837b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81840a01135b39bd443d",
+    "url": "/static/css/main.fd9ea794.chunk.css"
+  },
+  {
+    "revision": "482404af45e24689e280",
+    "url": "/static/js/2.abd5d619.chunk.js"
+  },
+  {
+    "revision": "81840a01135b39bd443d",
+    "url": "/static/js/main.22cba8fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
